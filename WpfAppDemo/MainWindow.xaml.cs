2d6c353671e1863e362e07494dcb176be048d3d7
pinkros <arvid.leimar@iths.se>
2022-10-06 11:09:36 +0200

    itty bitty commitieh

--- WpfAppDemo/MainWindow.xaml.cs
+++ WpfAppDemo/MainWindow.xaml.cs
@@ -8,7 +8,12 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
+//Övning 1: 
+//Skapa en enkel numpad med siffrorna 0-9
+//Siffran ska läggas på i MinText
 
+//Övning 2 (EXTRA): 
+//Alla knappar ska använda samma metod
 
 namespace WpfAppDemo
 {
@@ -26,28 +31,32 @@ namespace WpfAppDemo
 
         private void Knapp_Click(object sender, RoutedEventArgs e)
         {
-            TextEtt.Text = Input.Text;
-        }
-
-        private void KnappStor_Click(object sender, RoutedEventArgs e)
-        {
-            TextEtt.FontSize += 10;
-        }
-
-        private void KnappLiten_Click(object sender, RoutedEventArgs e)
-        {
-            TextEtt.FontSize -= 10;
+            if (e.Source is Button btn)
+            {
+                TextEtt.Text += btn.Content;
+            }
 
         }
 
-        private void KnappKul_Click(object sender, RoutedEventArgs e)
-        {
-            TextEtt.FontFamily = new FontFamily("Comic Sans MS");
-        }
+        //private void KnappStor_Click(object sender, RoutedEventArgs e)
+        //{
+        //    TextEtt.FontSize += 10;
+        //}
 
-        private void KnappTraakig_Click(object sender, RoutedEventArgs e)
-        {
-            TextEtt.FontFamily = new FontFamily("Consolas");
-        }
+        //private void KnappLiten_Click(object sender, RoutedEventArgs e)
+        //{
+        //    TextEtt.FontSize -= 10;
+
+        //}
+
+        //private void KnappKul_Click(object sender, RoutedEventArgs e)
+        //{
+        //    TextEtt.FontFamily = new FontFamily("Comic Sans MS");
+        //}
+
+        //private void KnappTraakig_Click(object sender, RoutedEventArgs e)
+        //{
+        //    TextEtt.FontFamily = new FontFamily("Consolas");
+        
     }
 }
